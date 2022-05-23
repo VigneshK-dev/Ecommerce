08eb034ed3513333ee17cfd2c72e776170a88842
VIG <vignesh@VIGs-MacBook-Pro.local>
2022-05-23 22:49:57 +0530

    home

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -8,7 +8,36 @@ import Carousel from '../Components/Carousel/Carousel'
 
 function Home() {
 
- 
+  
+
+   const cat = [
+     {
+       id:1,
+       title:"Women",
+       image:'https://cdn.shopify.com/s/files/1/0130/5041/3114/files/demo10_01_1024x.jpg?v=1542301303',
+       link:"/pages/Products"
+     
+   },
+   {
+    id:2,
+    title:"Men",
+    image:'https://cdn.shopify.com/s/files/1/0130/5041/3114/files/demo10_02_1024x.jpg?v=1542301303',
+    link:"/pages/Products"
+   },
+   {
+    id:3,
+    title:"Electronics",
+    image:'https://www.journal-theme.com/1/image/cache/catalog/journal3/slider/demo1/s1-960x450h.png.webp',
+    link:"/pages/Products"
+   },
+   {
+    id:4,
+    title:"Jewelery",
+    image:'https://cdn.shopify.com/s/files/1/0746/3089/t/2/assets/home_block_1_banner_right_image.jpg?v=141168198028992914021610215664',
+    link:"/pages/Products"
+   }
+  
+  ]
 
 
   return (
@@ -20,71 +49,25 @@ function Home() {
 
     <Row className='my-3'>
 
-
-           <Col  className="my-1" lg={6} xs={12} >
-           <Link to="/Pages/Products" >
+            {cat.map((item,index)=> (
+            <Col  key={index} className="my-1" lg={6} xs={12} >
+           <Link to={item.link} >
             <div className='org'>
 
-                <img src='https://cdn.shopify.com/s/files/1/0130/5041/3114/files/demo10_01_1024x.jpg?v=1542301303' className='img img-fluid' alt='women'/>
+                <img src={item.image} className='img img-fluid' alt='women'/>
 
                    <div className='layer'>
-                       <h5 className='text-center text-dark fw-bold'>Women</h5>
+                       <h5 className='text-center text-dark fw-bold'>{item.title}</h5>
                     </div>
                </div>
                </Link>
-           </Col>
+           </Col> ))}
 
-
-           <Col lg={6} xs={12} >
-           <Link to="/Pages/Products"  >
-           <div className='org'>
-         
-           <img src='https://cdn.shopify.com/s/files/1/0130/5041/3114/files/demo10_02_1024x.jpg?v=1542301303' className='img  img-fluid' alt='men'/>
-
-                   <div className='layer'>
-                       <h5 className='text-center text-dark  fw-bold'>Men</h5>
-                    </div>
-               </div>
-               </Link>
-           </Col>
-
-       
     </Row>
      
 
-      <Row>
-             
-      <Col lg={6} xs={12} >
-      <Link to="/Pages/Products" >
-            <div className='org'>
-
-                <img src='https://www.journal-theme.com/1/image/cache/catalog/journal3/slider/demo1/s1-960x450h.png.webp' className='img img-fluid' alt='ele'/>
-            
-                   <div className='layer'>
-                       <h5 className='text-center text-dark  fw-bold'>Electronics</h5>
-                    </div>
-               </div>
-               </Link>
-           </Col>
-
-
-           <Col lg={6} xs={12} >
-           <Link to="/Pages/Products" >
-           <div className='org'  >
-         
-           <img src='https://cdn.shopify.com/s/files/1/0746/3089/t/2/assets/home_block_1_banner_right_image.jpg?v=141168198028992914021610215664' className='img img-fluid' alt='jew'/>
-         
-                   <div className='layer'>
-                       <h5 className='text-center text-dark fw-bold'>jewelery</h5>
-                    </div>
-               </div>
-               </Link>
-           </Col>
-
-
-
-      </Row>
-
+      
+     
   </Container>
 
    
